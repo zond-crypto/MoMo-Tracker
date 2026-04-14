435b9833a1f77abb76ee7d8ad25f5ae1885b898b
zond-crypto <bandanorman19@gmail.com>
2026-04-14 15:13:12 +0200

    Added PWA support and offline functionality

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'momo-tracker-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
